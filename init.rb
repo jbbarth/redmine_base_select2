f83b46e61d7bbf1baeb1b508d7ef353db981baf5
Vincent Robert <vincent.robert@nanego.com>
2015-07-23 17:58:08 +0200

    Upgrade to select2 v4.0.0

--- init.rb
+++ init.rb
@@ -6,7 +6,7 @@ Redmine::Plugin.register :redmine_base_select2 do
   description 'This plugin adds the Select2 component to your Redmine instance'
   author 'Jean-Baptiste BARTH'
   author_url 'mailto:jeanbaptiste.barth@gmail.com'
-  version '0.0.1'
+  version '4.0.0'
   url 'https://github.com/jbbarth/redmine_base_select2'
   requires_redmine :version_or_higher => '2.1.0'
 end
